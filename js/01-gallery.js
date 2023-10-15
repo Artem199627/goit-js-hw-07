4158502898671506320db122fc49cc107b67904d
Artem199627 <artemishko@gmail.com>
2023-10-15 22:51:31 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markupImages =  galleryItems.map(({ preview, description, original }) =>  `<li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </li>`).join("");
+
+gallery.insertAdjacentHTML("beforeend", markupImages);
+gallery.addEventListener("click", onGalleryClick);
+function onGalleryClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+}
+
+
+
+
