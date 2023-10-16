60aefe96f228de3208f46d015fed4259eaabaa57
Artem199627 <artemishko@gmail.com>
2023-10-16 20:56:06 +0200

    lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+
+const markup = galleryItems.map(({ preview, description, original }) => `<li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          alt=${description}
+        />
+      </a>
+    </li>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const lightbox = new SimpleLightbox(".gallery__item a", {
+    captionsData: "alt",
+    captionDelay : 250,     
+});
+
+
