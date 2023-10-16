c4d6aee8d85ef88a5e5375b4de965757f511ed4b
Artem199627 <artemishko@gmail.com>
2023-10-16 20:21:17 +0200

    gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,26 +4,50 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
+let fullImages;
+let imagesInfo;
 
-const markupImages =  galleryItems.map(({ preview, description, original }) =>  `<li class="gallery__item">
+const markup = galleryItems.map(({ preview, description, original }) => `<li class="gallery__item">
       <a class="gallery__link" href=${original}>
         <img
           class="gallery__image"
           src=${preview}
-          data-source=${original}
           alt=${description}
+          data-source=${original}
         />
       </a>
     </li>`).join("");
 
-gallery.insertAdjacentHTML("beforeend", markupImages);
+gallery.insertAdjacentHTML("beforeend", markup);
 gallery.addEventListener("click", onGalleryClick);
+
 function onGalleryClick(evt) {
-    evt.preventDefault();
-    if (evt.target.nodeName !== "IMG") {
-        return;
-    }
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  imagesInfo = evt.target.dataset.source;
+  createLightBox();
 }
+function createLightBox() {
+  fullImages = basicLightbox.create(
+    `<img  src=${imagesInfo} />`, {
+    onShow: () => {
+      window.addEventListener("keydown", onKeyPress);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", onKeyPress);
+    },
+  }
+  );
+  fullImages.show();
+}
+function onKeyPress({code}) {
+  if (code === "Escape") {
+    fullImages.close();
+  }
+}
+
 
 
 
